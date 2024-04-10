76256e773d696d2f06847a9929f6f7fc2b2cb861
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-04-10 10:08:47 +0530

    Update thumbnails.py

--- SlayerX/utils/thumbnails.py
+++ SlayerX/utils/thumbnails.py
@@ -13,7 +13,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
-    newHeight = int(heightRatio * image.size[1])
+    newHeight = int(heightRatio * image.size[])
     newImage = image.resize((newWidth, newHeight))
     return newImage
 
