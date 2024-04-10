50326f054043eb5d0a0ff97d0441b792714a2db0
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-04-10 10:07:18 +0530

    Update __init__.py

--- SlayerX/__init__.py
+++ SlayerX/__init__.py
@@ -2,7 +2,7 @@ from SlayerX.core.bot import Slayer
 from SlayerX.core.dir import dirr
 from SlayerX.core.git import git
 from SlayerX.core.userbot import Userbot
-from SlayerX.misc import dbb, herok
+from SlayerX.misc import dbb, heroku
 
 from .logging import LOGGER
 
